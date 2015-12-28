3f7e6bb68e66a96c6a6e2b409ae90e897f382e84
Ryan Gooler <ryan.gooler@gmail.com>
2015-12-28 15:42:03 -0800

    Got reactions going

--- meteor_rps.js
+++ meteor_rps.js
@@ -1,4 +1,25 @@
+Game = new Mongo.Collection("game");
+
 if (Meteor.isClient) {
+  Session.setDefault('player', 0);
+
+  Template.player.helpers({
+    player: function () {
+      return Session.get("player");
+    }
+  });
+
+  Template.player.events({
+    'click button.rock': function () { 
+      console.log("Player " + Session.get('player') + " played rock");
+    },
+    'click button.paper': function () { 
+      console.log("Player " + Session.get('player') + " played paper");
+    },
+    'click button.scissors': function () { 
+      console.log("Player " + Session.get('player') + " played scissors");
+    },
+  });
 }
 
 if (Meteor.isServer) {
@@ -19,17 +40,11 @@ Router.route('/', {
 
 // Player pages
 Router.route('/player1', function() {
-  this.render('player', {
-    data: {
-      _player: 1
-    }
-  });
+  Session.set("player", 1);
+  this.render('player');
 });
 
 Router.route('/player2', function() {
-  this.render('player', {
-    data: {
-      _player: 2
-    }
-  });
+  Session.set("player", 2);
+  this.render('player');
 });
